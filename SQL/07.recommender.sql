3fa1379b6d12214685082ee5c418972274d24e1a
Risa Carvalho <carvalhorisa8@gmail.com>
2026-09-04 12:26:54 -0400

    Add top 10 job matches

--- SQL/07.recommender.sql
+++ SQL/07.recommender.sql
@@ -1,5 +1,6 @@
 --Problem 1: Given a sample portfolii user with skills `SQL, Python, Power BI, Data Visualization`, write a query that scores every job posting in the `jobs` table by how many of those 4 skills appear in its `required_skills` column, without excluding any jobs — then sort so the best matches appear first.
-SELECT job_id, title,required_skills,
+SELECT TOP 10
+             job_id, title,required_skills,
 (CASE WHEN required_skills LIKE '%SQL%' THEN 1 ELSE 0 END) +
     (CASE WHEN required_skills LIKE '%Python%' THEN 1 ELSE 0 END) +
     (CASE WHEN required_skills LIKE '%Power BI%' THEN 1 ELSE 0 END) +
