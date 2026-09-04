6b1a0f2598b542f01d7b7b9dcd299481477d9e43
Risa Carvalho <carvalhorisa8@gmail.com>
2026-09-03 23:32:09 -0400

    create table and insert data

---
+++ data/create_table.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS jobs_raw;
+GO
+
+CREATE TABLE jobs_raw (
+    row_num VARCHAR(50),
+    job_id VARCHAR(50),
+    title VARCHAR(255),
+    company VARCHAR(255),
+    location VARCHAR(255),
+    description VARCHAR(1000),
+    required_skills VARCHAR(500),
+    source_url VARCHAR(500)
+);
+GO
+
+BULK INSERT jobs_raw
+FROM 'C:\Users\carva\OneDrive\Desktop\Risa Data Projects\job-recommender\data\messy_jobs_raw.csv'
+WITH (FORMAT='CSV', FIRSTROW=2, FIELDQUOTE='"', FIELDTERMINATOR=',', ROWTERMINATOR='0x0a', CODEPAGE='65001', TABLOCK);
+
+select * from jobs_raw;
+
+SELECT COUNT(*) AS total_rows FROM jobs_raw;
